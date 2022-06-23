055bdf79758fb1439dc0bca21d2ea41c95324228
Aaron Cooke <807735+imaaron@users.noreply.github.com>
2022-06-23 22:47:53 +0100

    refactor gcs to bq dag

--- airflow/dags/gsc_bg_dag.py
+++ airflow/dags/gsc_bg_dag.py
@@ -1,18 +1,22 @@
 import os
 import logging
+from re import M
 
 from airflow import DAG
 from airflow.utils.dates import days_ago
 
 from airflow.providers.google.cloud.operators.bigquery import BigQueryCreateExternalTableOperator, BigQueryInsertJobOperator
 
-
-from datetime import datetime
-
 PROJECT_ID = os.environ.get("GCP_PROJECT_ID")
 BUCKET = os.environ.get("GCP_GCS_BUCKET")
 AIRFLOW_HOME = os.environ.get("AIRFLOW_HOME", "/opt/airflow/")
 BIGQUERY_DATASET = os.environ.get("BIGQUERY_DATASET", 'trips_data_all')
+
+TRIP_TYPES = {
+    'yellow': 'tpep_pickup_datetime',
+    # 'green': 'lpep_pickup_datetime',
+    # 'fhv': 'Pickup_datetime'
+}
 
 default_args = {
     "owner": "airflow",
@@ -21,12 +25,19 @@ default_args = {
     "retries": 1,
 }
 
-def gcs_to_bq_dag(
-    dag, trip_type, dtime_field
-):
-    with dag:
+with DAG(
+    dag_id="gcs_bg_tlc_data",
+    schedule_interval="@daily",
+    default_args=default_args,
+    catchup=False,
+    max_active_runs=1,
+    tags=['dtc-de'],
+) as dag:
+
+    for trip_type, dt_col in TRIP_TYPES.items():
+
         gcs_to_bq_ext_task = BigQueryCreateExternalTableOperator(
-            task_id="gsc_to_bg_ext_task",
+            task_id=f"gsc_to_bq_{trip_type}_ext_task",
             table_resource={
                 "tableReference": {
                     "projectId": PROJECT_ID,
@@ -35,19 +46,20 @@ def gcs_to_bq_dag(
                 },
                 "externalDataConfiguration": {
                     "sourceFormat": "PARQUET",
-                    "sourceUris": [f"gs://{BUCKET}/raw/{trip_type}_tripdata/2019/*"]
+                    "sourceUris": [f"gs://{BUCKET}/raw/{trip_type}_tripdata/2019/*"],
+                    "autodetect": True
                 }
             },
         )
 
         CREATE_PARTITION_TABLE = (
             f"CREATE OR REPLACE TABLE {BIGQUERY_DATASET}.{trip_type}_partitioned "
-            f"PARTITION BY DATE({dtime_field}) "
+            f"PARTITION BY DATE({dt_col}) "
             f"AS SELECT * FROM {BIGQUERY_DATASET}.{trip_type}_external;"
         )
 
         bg_partition_task = BigQueryInsertJobOperator(
-            task_id="bg_partition_task",
+            task_id=f"bq_{trip_type}_partition_task",
             configuration={
                 "query": {
                     "query": CREATE_PARTITION_TABLE,
@@ -57,51 +69,3 @@ def gcs_to_bq_dag(
         )
 
         gcs_to_bq_ext_task >> bg_partition_task
-
-# Yellow Taxi DAG
-gcs_bg_yellow_taxi_dag = DAG(
-    dag_id="gcs_bg_yellow_taxi_v0.3",
-    schedule_interval="0 6 3 * *",
-    default_args=default_args,
-    catchup=False,
-    max_active_runs=3,
-    tags=['dtc-de'],
-)
-
-gcs_to_bq_dag(
-    dag=gcs_bg_yellow_taxi_dag,
-    trip_type="yellow",
-    dtime_field='tpep_pickup_datetime',
-)
-
-# Green Taxi DAG
-gcs_bg_green_taxi_dag = DAG(
-    dag_id="gcs_bg_green_taxi_v1.1",
-    schedule_interval="0 7 3 * *",
-    default_args=default_args,
-    catchup=False,
-    max_active_runs=3,
-    tags=['dtc-de'],
-)
-
-gcs_to_bq_dag(
-    dag=gcs_bg_green_taxi_dag,
-    trip_type="green",
-    dtime_field='lpep_pickup_datetime',
-)
-
-# FHV Taxi DAG
-gcs_bg_fhv_taxi_dag = DAG(
-    dag_id="gcs_bg_fhv_taxi_v1.1",
-    schedule_interval="0 8 3 * *",
-    default_args=default_args,
-    catchup=False,
-    max_active_runs=3,
-    tags=['dtc-de'],
-)
-
-gcs_to_bq_dag(
-    dag=gcs_bg_fhv_taxi_dag,
-    trip_type="fhv",
-    dtime_field='tpep_pickup_datetime',
-)
